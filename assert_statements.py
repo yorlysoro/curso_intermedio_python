84dae5a4bc8662f50bcabd9271b09aefa5862a26
yorlysoro <yorlysoro@gmail.com>
2022-07-14 11:01:02 -0400

    end of intermediate course

---
+++ assert_statements.py
@@ -0,0 +1,20 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = input('Ingresa un número: ')
+    assert num.isnumeric(), "Debes ingresar un número"
+    print(divisors(int(num)))
+    print("Terminó mi programa")
+
+
+if __name__ == '__main__':
+    try:
+        run()
+    except Exception as e:
+        print(e)
